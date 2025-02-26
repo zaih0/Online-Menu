a705cbc6af7bd7f656f2bc9f2c674eaf83439f32
Hayden <noaringhs156@gmail.com>
2025-02-26 11:08:08 +0100

    ....

--- index.php
+++ index.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu</title>
+    <link rel="stylesheet" href="./css/index.css">
 </head>
 <body>
-    <button><a href="opening-times.php">Openingstijden</a></button>
-    
+    <div id="topbalk">
+        <div class="menu">
+            <div class="stripe1 stripe"></div>
+            <div class="stripe2 stripe"></div>
+            <div class="stripe3 stripe"></div>
+        </div>
+        <div class="logo"><img src="./logo-mees/logo-mees.svg" alt="MEES??"></div>
+        <a href="opening-times.php" class="opening-times"><h3>Openingstijden</h3></a>
+    </div>
+
+    <div></div>
 </body>
 </html>
